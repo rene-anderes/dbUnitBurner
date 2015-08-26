a74eda5f35253600cfab267ac5ff81cdfaf3cadc
René Anderes <randeres@yahoo.de>
2015-08-26 12:56:58 +0200

    Initialer Import

---
+++ src/main/java/org/anderes/edu/dbunitburner/CustomDataTypeFactory.java
@@ -0,0 +1,19 @@
+package org.anderes.edu.dbunitburner;
+
+import java.sql.Types;
+
+import org.dbunit.dataset.datatype.DataType;
+import org.dbunit.dataset.datatype.DataTypeException;
+import org.dbunit.dataset.datatype.DefaultDataTypeFactory;
+
+public class CustomDataTypeFactory extends DefaultDataTypeFactory {
+    
+    public DataType createDataType(int sqlType, String sqlTypeName) throws DataTypeException {
+        System.out.println("--------------------------" + sqlTypeName);
+        if (sqlType == Types.TIMESTAMP) {
+            return new CustomTimestampDataType();
+        } else {
+            return super.createDataType(sqlType, sqlTypeName);
+        }
+    }
+}
